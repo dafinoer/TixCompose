ac516069795afe15361fe0f55e92ad1cc7c0381b
Muhammad Dafi <dafinoer@outlook.com>
2023-09-15 22:22:37 +0700

    refactor kotlin flow

--- app/src/main/java/com/dafinrs/tixcompose/presentations/nowplaying/NowPlayingViewModel.kt
+++ app/src/main/java/com/dafinrs/tixcompose/presentations/nowplaying/NowPlayingViewModel.kt
@@ -1,22 +1,15 @@
 package com.dafinrs.tixcompose.presentations.nowplaying
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.dafinrs.tixcompose.domain.model.MovieModel
 import com.dafinrs.tixcompose.domain.usecases.GetNowPlayingUseCase
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.shareIn
-import kotlinx.coroutines.flow.switchMap
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.stateIn
 import org.koin.android.annotation.KoinViewModel
 import org.koin.core.annotation.InjectedParam
 
@@ -25,7 +18,7 @@ import org.koin.core.annotation.InjectedParam
 class NowPlayingViewModel(@InjectedParam private val nowPlayingUseCase: GetNowPlayingUseCase) :
     ViewModel() {
 
-    private val nowPlayingPage = MutableStateFlow(1);
+    private val nowPlayingPage = MutableStateFlow(1)
 
     @OptIn(ExperimentalCoroutinesApi::class)
     val nowPlayingUiState = nowPlayingPage.flatMapLatest { value ->
@@ -33,13 +26,13 @@ class NowPlayingViewModel(@InjectedParam private val nowPlayingUseCase: GetNowPl
             emit(NowPlayingState.Loading)
             val result = nowPlayingUseCase.call(value)
             emit(NowPlayingState.Success(result))
-        }.catch {
-            emit(NowPlayingState.Error())
         }
-    }.shareIn(
+    }.catch {
+        emit(NowPlayingState.Error())
+    }.stateIn(
         scope = viewModelScope,
         started = SharingStarted.WhileSubscribed(5_000),
-        replay = 1
+        initialValue = NowPlayingState.Loading
     )
 
     init {
