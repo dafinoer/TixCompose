a20ba4aaa291dce1bd3cd7f47c230c74353ec4f4
Muhammad Dafi <dafinoer@outlook.com>
2023-09-17 16:52:54 +0700

    add more parameter

--- app/src/main/java/com/dafinrs/tixcompose/ui/pages/base/BaseScreenPage.kt
+++ app/src/main/java/com/dafinrs/tixcompose/ui/pages/base/BaseScreenPage.kt
@@ -6,21 +6,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.dafinrs.tixcompose.domain.model.CinemasModel
 import com.dafinrs.tixcompose.domain.usecases.GetListLocationCinema
 import com.dafinrs.tixcompose.domain.usecases.GetNowPlayingUseCase
 import com.dafinrs.tixcompose.presentations.cinemalocation.CinemaLocationViewModel
-import com.dafinrs.tixcompose.presentations.nowplaying.NowPlayingState
 import com.dafinrs.tixcompose.presentations.nowplaying.NowPlayingViewModel
 import com.dafinrs.tixcompose.ui.pages.base.components.bottomnav.BottomNavBasePage
 import com.dafinrs.tixcompose.ui.pages.cinema.CinemaScreenPage
@@ -59,36 +51,6 @@ fun BaseScreenPage(
     onTapDetailCard: (String) -> Unit,
 ) {
     val rememberNavigationBarController = rememberNavController()
-    val nowPlayingMovieState = nowPlayingViewModel.nowPlayingUiState.collectAsStateWithLifecycle(
-        initialValue = NowPlayingState.Initial,
-    )
-    var cinemaType by remember { mutableStateOf<CinemasModel?>(null) }
-    val cinemaLocationState = locationViewModel.getLocationUserFlow.collectAsStateWithLifecycle()
-
-    val nowPlayingValues by remember {
-        derivedStateOf {
-            when (nowPlayingMovieState.value) {
-                is NowPlayingState.Success -> {
-                    val successState = nowPlayingMovieState.value
-                    if (successState is NowPlayingState.Success) {
-                        val newItems =
-                            successState.items.filter { it.cinemaList.contains(cinemaType) }
-                        if (newItems.isNotEmpty()) {
-                            successState.copy(items = newItems)
-                        } else {
-                            successState
-                        }
-                    } else {
-                        nowPlayingMovieState.value
-                    }
-                }
-
-                else -> nowPlayingMovieState.value
-            }
-        }
-    }
-
-
     Scaffold(
         bottomBar = {
             BottomNavBasePage(rememberNavigationBarController)
@@ -110,13 +72,15 @@ fun BaseScreenPage(
                     onSearchBar = onSearchBar,
                     onGoToProfile = onGoToProfile,
                     onGoToNotification = onGoToNotification,
-                    onDetail = onTapDetailCard,
-                    nowPlayingState = nowPlayingValues,
-                    onTapButtonCinemas = { cinemaType = it },
+                    nowPlayingViewModel = nowPlayingViewModel,
+                    cinemaLocationViewModel = locationViewModel,
                     onClickLocation = onTapLocation,
-                    cinemaType = cinemaType,
-                    locationName = cinemaLocationState.value?.name
-                )
+                    onMoreMovie = {
+                        /// TODO: navigate to more list movies
+                    },
+                ) {
+                    onTapDetailCard(it)
+                }
             }
             composable("cinema") {
                 CinemaScreenPage()
