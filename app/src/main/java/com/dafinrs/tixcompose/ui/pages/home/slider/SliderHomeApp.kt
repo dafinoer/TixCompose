c9ddf833566701ab048fbed405b3bcc010fcee2b
Muhammad Dafi <dafinoer@outlook.com>
2023-09-17 16:59:58 +0700

    refactor code

--- app/src/main/java/com/dafinrs/tixcompose/ui/pages/home/slider/SliderHomeApp.kt
+++ app/src/main/java/com/dafinrs/tixcompose/ui/pages/home/slider/SliderHomeApp.kt
@@ -6,25 +6,15 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dafinrs.tixcompose.R
@@ -32,20 +22,12 @@ import com.dafinrs.tixcompose.R
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun SliderHomeApp(modifier: Modifier = Modifier) {
-    val currentIndex = remember {
-        mutableIntStateOf(0)
-    }
+fun SliderHomeApp() {
     val rememberPageState = rememberPagerState {
         5
     }
-
-    LaunchedEffect(rememberPageState.currentPage) {
-        currentIndex.intValue = rememberPageState.currentPage
-    }
-
     Box(
-        modifier = modifier
+        modifier = Modifier
             .padding(vertical = 16.dp)
             .aspectRatio(1.7f)
             .fillMaxSize()
