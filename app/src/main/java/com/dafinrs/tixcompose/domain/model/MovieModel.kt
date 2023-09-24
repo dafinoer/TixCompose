7a0cd67fa1d3345edc84b0801346bdeb6d888b7c
Muhammad Dafi <dafinoer@outlook.com>
2023-09-24 17:02:26 +0700

    change to zoneddate

--- app/src/main/java/com/dafinrs/tixcompose/domain/model/MovieModel.kt
+++ app/src/main/java/com/dafinrs/tixcompose/domain/model/MovieModel.kt
@@ -1,11 +1,12 @@
 package com.dafinrs.tixcompose.domain.model
 
-import java.time.LocalDate
+
+import java.time.ZonedDateTime
 
 data class MovieModel(
     val id: Int,
     val title: String,
-    val releaseDate: LocalDate,
+    val releaseDate: ZonedDateTime,
     val voteCount: Int = 0,
     val desc: String? = null,
     val votes: Double = 0.0,
