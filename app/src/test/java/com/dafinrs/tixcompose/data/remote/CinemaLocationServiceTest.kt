239df44be84d298487433676678c71c4cc3784e2
Muhammad Dafi <dafinoer@outlook.com>
2023-09-24 16:56:49 +0700

    create mockk test

--- app/src/test/java/com/dafinrs/tixcompose/data/remote/CinemaLocationServiceTest.kt
+++ app/src/test/java/com/dafinrs/tixcompose/data/remote/CinemaLocationServiceTest.kt
@@ -1,6 +1,9 @@
 package com.dafinrs.tixcompose.data.remote
 
+import com.dafinrs.tixcompose.utilities.CinemaLocationFile
 import com.squareup.moshi.Moshi
+import io.mockk.coEvery
+import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert
 import org.junit.Before
@@ -10,17 +13,23 @@ class CinemaLocationServiceTest {
 
     lateinit var locationService: CinemaLocationService
     lateinit var moshi: Moshi
+    lateinit var cinemaLocationFile: CinemaLocationFile
 
     @Before
     fun onSetRun() {
         moshi = Moshi.Builder().build()
-       locationService = CinemaLocationService(moshi)
+        cinemaLocationFile = mockk(relaxed = true)
+        locationService = CinemaLocationService(moshi, cinemaLocationFile)
     }
 
     @Test
     fun getLocationTest() = runTest {
+        coEvery { cinemaLocationFile.readCinemaLocationFile() } returns """
+            [{"id": 2, "title": "Bogor"}]
+        """.trimIndent()
         val result = locationService.getCinemaLocation()
-        Assert.assertEquals(result.size, 6)
-        Assert.assertEquals(result[0].id, 0)
+        coEvery { cinemaLocationFile.readCinemaLocationFile() }
+
+        Assert.assertEquals(result.size, 1)
     }
 }
