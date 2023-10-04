e456eef21a6043ca368f68deebbaa5aa687fdc4d
Muhammad Dafi <dafinoer@outlook.com>
2023-10-04 22:20:51 +0700

    add splash screen api

--- app/src/main/java/com/dafinrs/tixcompose/MainActivity.kt
+++ app/src/main/java/com/dafinrs/tixcompose/MainActivity.kt
@@ -3,6 +3,13 @@ package com.dafinrs.tixcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -13,20 +20,34 @@ import com.dafinrs.tixcompose.ui.pages.detail.detailPage
 import com.dafinrs.tixcompose.ui.pages.locations.LocationScreenPage
 import com.dafinrs.tixcompose.ui.theme.TixComposeTheme
 import com.dafinrs.tixcompose.utilities.NavigationPageKey
+import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
 
 class MainActivity : ComponentActivity() {
     private val getApiToken by inject<GetApiToken>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splash = installSplashScreen()
+        var isHaveToken by mutableStateOf<Boolean?>(null)
         super.onCreate(savedInstanceState)
-        val isHaveApiToken = getApiToken.call() != null
+
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                val resultToken = getApiToken.call()
+                isHaveToken = resultToken != null
+            }
+        }
+
+        splash.setKeepOnScreenCondition {
+            isHaveToken == null
+        }
 
         setContent {
             TixComposeTheme {
                 val navController = rememberNavController()
                 NavHost(
-                    navController = navController, startDestination = startFirstUrl(isHaveApiToken),
+                    navController = navController,
+                    startDestination = startFirstUrl(isHaveToken ?: false),
                 ) {
                     composable(NavigationPageKey.API_KEY_PAGE) {
                         ApiKeyScreenPage {
