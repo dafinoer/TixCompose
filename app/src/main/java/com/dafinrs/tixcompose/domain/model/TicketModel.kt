d44b261b19d22ea50a7f9705bde3191f96d2d411
Muhammad Dafi <dafinoer@outlook.com>
2023-10-06 17:48:50 +0700

    refactor code

--- app/src/main/java/com/dafinrs/tixcompose/domain/model/TicketModel.kt
+++ app/src/main/java/com/dafinrs/tixcompose/domain/model/TicketModel.kt
@@ -6,10 +6,11 @@ data class TicketModel(
     val ticketId: Int,
     val movieId: Int,
     val title: String,
-    val desc: String,
-    val imageUrl: String,
     val totalQty: Int = 0,
     val movieDate: ZonedDateTime?,
     val ticketStatus: TicketStatusModel? = null,
     val createdTicketDate: ZonedDateTime,
+    val totalPrice: Long = 0,
+    val imageUrl: String,
 )
+
