9833156063e383cb17632acec37c47ca5ebb4730
Muhammad Dafi <dafinoer@outlook.com>
2023-10-06 17:48:09 +0700

    refactor

--- app/src/main/java/com/dafinrs/tixcompose/data/models/locals/FavMovieLocal.kt
+++ app/src/main/java/com/dafinrs/tixcompose/data/models/locals/FavMovieLocal.kt
@@ -4,22 +4,22 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.dafinrs.tixcompose.domain.model.MovieModel
+import com.dafinrs.tixcompose.utilities.toDate
 import com.dafinrs.tixcompose.utilities.toZoneDateTime
 import java.util.Date
 
-@Entity(tableName = "movie")
-data class MovieLocal(
+@Entity(tableName = "favorite")
+data class FavMovieLocal(
     @PrimaryKey val uid: Int,
     val title: String,
-    val desc: String,
-    @ColumnInfo(name = "image_url") val posterImageUrl: String,
+    val desc: String?,
+    @ColumnInfo(name = "image_url") val posterImageUrl: String?,
     @ColumnInfo(name = "vote_count") val voteCount: Int = 0,
     @ColumnInfo(name = "vote_average") val voteAverage: Double = 0.0,
-    @ColumnInfo(name = "is_favorite") val isFavorite: Boolean = false,
     @ColumnInfo(name = "releaseDate") val releaseDate: Date,
 )
 
-fun MovieLocal.toEntity() = MovieModel(
+fun FavMovieLocal.toEntity() = MovieModel(
     id = uid,
     title = title,
     desc = desc,
@@ -29,3 +29,12 @@ fun MovieLocal.toEntity() = MovieModel(
     voteCount = voteCount,
 )
 
+fun MovieModel.toFavMovieLocal() = FavMovieLocal(
+    uid = id,
+    title = title,
+    desc = desc,
+    posterImageUrl = postPath,
+    voteCount = voteCount,
+    voteAverage = votes,
+    releaseDate = releaseDate.toDate(),
+)
